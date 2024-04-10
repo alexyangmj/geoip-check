ff45ca20d23f4e27eaef471056180855ee1a3603
Alex Yang <Undisclosed@localhost>
2024-04-10 13:09:15 +0800

    v1.1

--- geoip-check.go
+++ geoip-check.go
@@ -12,20 +12,59 @@ import (
 )
 
 func main() {
-    Banner := "geoip-check v1.0\n"
+    Banner := "geoip-check v1.1\n"
     Banner = Banner + "Last Update: 10 Apr 2024, Alex Yang (linkedin.com/in/4yang)\n\n"
-    Banner = Banner + "Usage: geoip-check [IPv4/v6]\n"
-    Banner = Banner + "Usage: geoip-check inputfile.txt (file extension must be .txt)\n"
+    Banner = Banner + "Usage for Single IP query:\n"
+    Banner = Banner + "   geoip-check [IPv4/v6] [Optional_Switch]\n\n"
+    Banner = Banner + "Optional_Switch for output format:\n"
+    Banner = Banner + "0   Suppresss showing source IP\n"
+    Banner = Banner + "I   Show only source IP, Country ISO Code\n"
+    Banner = Banner + "C   Show only source IP, Country\n"
+    Banner = Banner + "c   Show only source IP, City\n"
+    Banner = Banner + "T   Show only source IP, Timezone\n"
+    Banner = Banner + "L   Show only source IP, Latitude, Longitude\n"
+    Banner = Banner + "Cc  Show only source IP, Country, City\n\n"
+    Banner = Banner + "Example:\n"
+    Banner = Banner + "   geoip-check 74.125.200.100\n"
+    Banner = Banner + "   geoip-check 2607:f8b0:4003:0c00:0000:0000:0000:006a\n"
+    Banner = Banner + "   geoip-check 74.125.200.101 Cc\n"
+    Banner = Banner + "   geoip-check 74.125.200.101 0C\n\n"
+    Banner = Banner + "Usage for Bulk IP query:\n"
+    Banner = Banner + "   geoip-check [inputfile.txt] --> file extension must be .txt\n\n"
+    Banner = Banner + "Example:\n"
+    Banner = Banner + "   geoip-check input.txt\n"
+    Banner = Banner + "   geoip-check input.txt 0Cc\n\n"
     
+    var inputIP     string
+    var Switch      string
+
     isFile := false
-    
+    Switch = "NIL"
+
     defer func() {
-        if r := recover(); r != nil { 
+        if r := recover(); r != nil {
+            fmt.Println("We go to recover function!")
             fmt.Println(Banner) 
         }
     }()
+
+    fmt.Printf("Args Len: %v\n", len(os.Args))
+
+    if len(os.Args) == 1  { 
+        fmt.Println(Banner)
+        return
+    } 
     
-    inputIP := os.Args[1]
+    if len(os.Args) == 2 { inputIP = os.Args[1] } 
+
+    if len(os.Args) == 3 { 
+        inputIP = os.Args[1]
+        Switch  = os.Args[2] 
+    }
+    
+    fmt.Printf("Input IP: %v\n", inputIP)
+    fmt.Printf("Switch Value: %v\n", Switch)
+    
     if !strings.Contains(inputIP, ".") && !strings.Contains(inputIP, ":") {
         fmt.Println(Banner)
         return
@@ -81,13 +120,57 @@ func main() {
                 errline++
                 continue
             }
-            fmt.Printf("%v,", txtlines)
-            fmt.Printf("%v,", record.Country.IsoCode)
-            fmt.Printf("%v,", record.Country.Names["en"])
-            fmt.Printf("%v,", record.City.Names["en"])
-            fmt.Printf("%v,", record.Location.TimeZone)
-            fmt.Printf("%v,", record.Location.Latitude)
-            fmt.Printf("%v\n", record.Location.Longitude)
+            switch Switch {
+            case "0":
+                fmt.Printf("%v,", record.Country.IsoCode)
+                fmt.Printf("%v,", record.Country.Names["en"])
+                fmt.Printf("%v,", record.City.Names["en"])
+                fmt.Printf("%v,", record.Location.TimeZone)
+                fmt.Printf("%v,", record.Location.Latitude)
+                fmt.Printf("%v\n", record.Location.Longitude)    
+            case "I":
+                fmt.Printf("%v,", txtlines)
+                fmt.Printf("%v\n", record.Country.IsoCode)
+            case "C":
+                fmt.Printf("%v,", txtlines)
+                fmt.Printf("%v\n", record.Country.Names["en"])
+            case "c":
+                fmt.Printf("%v,", txtlines)
+                fmt.Printf("%v\n", record.City.Names["en"])
+            case "T":
+                fmt.Printf("%v,", txtlines)
+                fmt.Printf("%v\n", record.Location.TimeZone)
+            case "L":
+                fmt.Printf("%v,", txtlines)
+                fmt.Printf("%v,", record.Location.Latitude)
+                fmt.Printf("%v\n", record.Location.Longitude)
+            case "Cc":
+                fmt.Printf("%v,", txtlines)
+                fmt.Printf("%v,", record.Country.Names["en"])
+                fmt.Printf("%v\n", record.City.Names["en"])
+            case "0C":
+                fmt.Printf("%v\n", record.Country.Names["en"])
+            case "0I":
+                fmt.Printf("%v\n", record.Country.IsoCode)                
+            case "0c":
+                fmt.Printf("%v\n", record.City.Names["en"])
+            case "0T":
+                fmt.Printf("%v\n", record.Location.TimeZone)
+            case "0L":
+                fmt.Printf("%v,", record.Location.Latitude)
+                fmt.Printf("%v\n", record.Location.Longitude)
+            case "0Cc":
+                fmt.Printf("%v,", record.Country.IsoCode)
+                fmt.Printf("%v\n", record.City.Names["en"])
+            case "NIL":
+                fmt.Printf("%v,", txtlines)
+                fmt.Printf("%v,", record.Country.IsoCode)
+                fmt.Printf("%v,", record.Country.Names["en"])
+                fmt.Printf("%v,", record.City.Names["en"])
+                fmt.Printf("%v,", record.Location.TimeZone)
+                fmt.Printf("%v,", record.Location.Latitude)
+                fmt.Printf("%v\n", record.Location.Longitude)
+            }
             errline++
         }
         file.Close()
@@ -97,13 +180,58 @@ func main() {
         if err != nil {
             log.Fatal(err)
         }
-        fmt.Printf("%v,", inputIP)
-        fmt.Printf("%v,", record.Country.IsoCode)
-        fmt.Printf("%v,", record.Country.Names["en"])
-        fmt.Printf("%v,", record.City.Names["en"])
-        fmt.Printf("%v,", record.Location.TimeZone)
-        fmt.Printf("%v,", record.Location.Latitude)
-        fmt.Printf("%v\n", record.Location.Longitude)
+
+        switch Switch {
+        case "0":
+            fmt.Printf("%v,", record.Country.IsoCode)
+            fmt.Printf("%v,", record.Country.Names["en"])
+            fmt.Printf("%v,", record.City.Names["en"])
+            fmt.Printf("%v,", record.Location.TimeZone)
+            fmt.Printf("%v,", record.Location.Latitude)
+            fmt.Printf("%v\n", record.Location.Longitude)    
+        case "I":
+            fmt.Printf("%v,", inputIP)
+            fmt.Printf("%v\n", record.Country.IsoCode)
+        case "C":
+            fmt.Printf("%v,", inputIP)
+            fmt.Printf("%v\n", record.Country.Names["en"])
+        case "c":
+            fmt.Printf("%v,", inputIP)
+            fmt.Printf("%v\n", record.City.Names["en"])
+        case "T":
+            fmt.Printf("%v,", inputIP)
+            fmt.Printf("%v\n", record.Location.TimeZone)
+        case "L":
+            fmt.Printf("%v,", inputIP)
+            fmt.Printf("%v,", record.Location.Latitude)
+            fmt.Printf("%v\n", record.Location.Longitude)
+        case "Cc":
+            fmt.Printf("%v,", inputIP)
+            fmt.Printf("%v,", record.Country.Names["en"])
+            fmt.Printf("%v\n", record.City.Names["en"])
+        case "0C":
+            fmt.Printf("%v\n", record.Country.Names["en"])
+        case "0I":
+            fmt.Printf("%v\n", record.Country.IsoCode)
+        case "0c":
+            fmt.Printf("%v\n", record.City.Names["en"])
+        case "0T":
+            fmt.Printf("%v\n", record.Location.TimeZone)
+        case "0L":
+            fmt.Printf("%v,", record.Location.Latitude)
+            fmt.Printf("%v\n", record.Location.Longitude)
+        case "0Cc":
+            fmt.Printf("%v,", record.Country.Names["en"])
+            fmt.Printf("%v\n", record.City.Names["en"])
+        case "NIL":
+            fmt.Printf("%v,", inputIP)
+            fmt.Printf("%v,", record.Country.IsoCode)
+            fmt.Printf("%v,", record.Country.Names["en"])
+            fmt.Printf("%v,", record.City.Names["en"])
+            fmt.Printf("%v,", record.Location.TimeZone)
+            fmt.Printf("%v,", record.Location.Latitude)
+            fmt.Printf("%v\n", record.Location.Longitude)
+        }
     }
 }
 
